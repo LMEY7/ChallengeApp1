85759c577c3412cc4a894f352a09dc544cb8583b
LMEY7 <meyger@onet.pl>
2023-11-26 16:09:00 +0100

    Zadanie dodatkowe dzień 4.

---
+++ ChallengeApp1/ChallengeApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Zadanie dodatkowe.
+
+var name1 = "Ewa";
+var woman = true;
+var age1 = 33;
+
+if(name1 == "Ewa" && age1 == 30)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if(woman && age1 < 30)
+{
+    Console.WriteLine("Kobieta, poniżej 30 lat");
+}
+else if(woman == true && name1 == "Ewa" && age1 == 33 && woman == true && age1 > 30 && !woman == false && age1 != 18)
+{
+    Console.WriteLine("Ewa lat 33 i jej koleżanka poniżej 30 lat rozmawiały z niepełnoletnim mężczyzną");
+}
+else
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
